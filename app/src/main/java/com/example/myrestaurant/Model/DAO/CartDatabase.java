821da7c635e68db6d4d653b2834b1c65a4c68bba
Mo7med EISSA <eissa1005@gmail.com>
2019-11-27 08:48:06 +0200

    ADDED
    Signed-off-by: Mo7med EISSA <eissa1005@gmail.com>

---
+++ app/src/main/java/com/example/myrestaurant/Model/DAO/CartDatabase.java
@@ -0,0 +1,27 @@
+package com.example.myrestaurant.Model.DAO;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities =CartItem.class,version = 1,exportSchema = false)
+public abstract class CartDatabase extends RoomDatabase {
+     public abstract CartDAO cartDAO();
+
+     private static CartDatabase cartInstance;
+     private static final String dbName="DbRestaurant";
+     public static CartDatabase getInstance(Context context){
+         if(cartInstance == null){
+             cartInstance = Room.databaseBuilder(context,CartDatabase.class,dbName)
+                     .fallbackToDestructiveMigration()
+                     .allowMainThreadQueries()
+                     .build();
+         }
+         return cartInstance;
+     }
+
+
+
+}
